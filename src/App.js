556a7ae05355763485b8a62825644ba5f8ee796c
isuapp <isaacmarti.g@gmail.com>
2022-06-18 12:46:22 +0200

    add to-do in progress column done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import Form from './components/form/Form';
 import Header from './components/header/Header';
 import ToDoes from './components/toDoes/toDoes';
+import ToDoesInProgress from './components/toDoesInProgress/ToDoesInProgress';
 
 function App() {
 
-  const [toDoes, setToDoes] = useState([])
+  const [toDoes, setToDoes] = useState([]);
+
+  const [toDoesInProgress, setToDoesInProgress] = useState([])
   
 
   // Remember the handle submit on the form component.
@@ -17,18 +20,42 @@ function App() {
     const newToDo = {
       id:nanoid(),
       name:inputValue,
+      toDo:true,
+      InProgress:false,
       complete:false,
     }
 
     // We upodate the list of to-does
     setToDoes([...toDoes, newToDo])
   }
+  const start = (id) =>{
+   
+      // Identify the toDo with the same id as the target
+      const [newInProgress] = toDoes.filter(toDo=>toDo.id===id);
+
+      //Identificamos todos los toDoes con id difente
+      const filteredToDoes = toDoes.filter(toDo=>toDo.id!==id);
+      
+
+      // Update the progress column with the new to-do
+      setToDoesInProgress([...toDoesInProgress, newInProgress]);
+
+      // We update the to-do column after the update of progress column
+      setToDoes(filteredToDoes)
+  }
+
 
   return (
     <div>
         <Header />
         <Form add={add}/>
-        <ToDoes toDoes={toDoes}/>
+        <div className='board'>
+          <ToDoes 
+            toDoes={toDoes} 
+            start={start}
+          />
+          <ToDoesInProgress toDoesInProgress={toDoesInProgress}/>
+        </div>
     </div>
   );
 }
--- src/components/toDo/ToDo.js
+++ src/components/toDo/ToDo.js
@@ -3,13 +3,17 @@ import './todo.css';
 
 // The toDo is the task it self on the toDos variable called 
 // on app.js
-const ToDo = ({toDo})=>{
+const ToDo = ({toDo, start})=>{
 
+    const startTask = (e)=>{
+        e.preventDefault();
+        start(toDo.id);
+    }
     return(
         <div className='todo' >
             <p>{toDo.name}</p>
             <div>
-                <button>delete</button>
+                <button onClick={startTask}>start</button>
                 <button>modify</button>
             </div>
         </div>
